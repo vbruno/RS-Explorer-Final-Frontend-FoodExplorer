7e263246d44565393754a50dec14b16291854292
Bruno S Velho <bruno.velho@gmail.com>
2023-11-05 14:32:49 -0300

    commit init project!! 🚀

---
+++ src/pages/Blank/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export function Blank() {
+  return (
+    <Container>
+      <h1>Pagina em branco</h1>
+    </Container>
+  );
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,34 @@
+export default {
+  COLORS: {
+    DARK_0100: "#000405",
+    DARK_0200: "#00070A",
+    DARK_0300: "#000204",
+    DARK_0400: "#000A0F",
+    DARK_0500: "#000C12",
+    DARK_0600: "#00111A",
+    DARK_0700: "#001119",
+    DARK_0800: "#0D161B",
+    DARK_0900: "#0D1D25",
+    DARK_1000: "#192227",
+
+    LIGHT_100: "#FFFFFF",
+    LIGHT_200: "#FFFAF1",
+    LIGHT_300: "#E1E1E6",
+    LIGHT_400: "#C4C4CC",
+    LIGHT_500: "#7C7C8A",
+    LIGHT_600: "#76797B",
+    LIGHT_700: "#4D585E",
+
+    CAKE_100: "#065E7C",
+    CAKE_200: "#82F3FF",
+
+    CARROT_100: "#FBA94C",
+
+    MINT_100: "#04D361",
+
+    TOMATO_100: "#750310",
+    TOMATO_200: "#92000E",
+    TOMATO_300: "#AB222E",
+    TOMATO_400: "#AB4D55",
+  },
+};
